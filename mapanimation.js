7063f61c0b3beacc07eb4ab39f5ab8e8f498841a
Jade Abel <jade.marie.abel@gmail.com>
2021-04-24 20:54:46 -0400

    js edits

--- mapanimation.js
+++ mapanimation.js
@@ -1,1 +1,35 @@
-<script></script>;
+mapboxgl.accessToken =
+  "pk.eyJ1IjoiaHVoLXd1aCIsImEiOiJja25jZ3J3NXoxejB3MnFxZHNsZWlnMWpoIn0.W85mfcYIMwtjtm4CWyY1Mg";
+var map = new mapboxgl.Map({
+  container: "map",
+  style: "mapbox://styles/mapbox/streets-v11",
+  center: [-71.104081, 42.365554],
+  zoom: 14,
+});
+
+var marker = new mapboxgl.Marker()
+  .setLngLat([-71.092761, 42.357575])
+  .addTo(map);
+const busStops = [
+  [-71.093729, 42.359244],
+  [-71.094915, 42.360175],
+  [-71.09558, 42.360698],
+  [-71.103476, 42.365248],
+  [-71.106067, 42.366806],
+  [-71.108717, 42.368355],
+  [-71.110799, 42.369192],
+  [-71.113095, 42.370218],
+  [-71.115476, 42.372085],
+  [-71.117585, 42.373016],
+  [-71.118625, 42.374863],
+];
+
+var counter = 0;
+function move() {
+  setTimeout(() => {
+    if (counter >= busStops.length) return;
+    marker.setLngLat(busStops[counter]);
+    counter++;
+    move();
+  }, 1000);
+}
